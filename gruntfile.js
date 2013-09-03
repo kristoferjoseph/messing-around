da8cab01b86d38faf179bbf645a1df70c02402de
kristoferjoseph <kristoferjoseph@gmail.com>
2013-09-03 14:01:35 -0700

    Initial pass

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,9 @@
-var shell = require('shelljs')
-,   harp  = require('harp')
-,   path  = require('path')
+var shell = require('shelljs'),
+    harp  = require('harp'),
+    path  = require('path');
 
 module.exports = function(grunt) {
-    
+
     // init ceremonies
     //
     grunt.initConfig({
@@ -13,36 +13,44 @@ module.exports = function(grunt) {
             'www/x.js': ['lib/index.js']
         },
 
+        topcoat: {
+          options: {
+            theme: 'mobile',
+            variation: 'light',
+            browsers: ['android']
+          }
+        },
+
         watch: {
             files: ["lib/**/*.js", 'test/**/*.js'],
             tasks: ['browserify', 'test']
         },
 
         concurrent: {
-            target: { 
+            target: {
                 tasks:['serve', 'watch'],
                 options:{logConcurrentOutput: true}
             }
         }
-    })
-    
-    grunt.loadNpmTasks('grunt-browserify')
-    grunt.loadNpmTasks('grunt-contrib-watch')
-    grunt.loadNpmTasks('grunt-concurrent')
+    });
 
-    // custom business 
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    // custom business
     //
-    grunt.registerTask('default', ['concurrent:target'])
-    
+    grunt.registerTask('default', ['concurrent:target']);
+
     grunt.registerTask('test', 'Run the unit tests.', function() {
-        shell.exec('npm test')
-    })
+        shell.exec('npm test');
+    });
 
     grunt.registerTask('serve', 'Serve www with Harp.', function() {
-        var done = this.async()
-        var projectPath = path.join(__dirname, 'www')
+        var done = this.async();
+        var projectPath = path.join(__dirname, 'www');
         harp.server(projectPath, [], function cb() {
-            grunt.log.write("\nHarp server is now running at http://localhost:9966\n\n")
-        })        
-    })
-}
+            grunt.log.write("\nHarp server is now running at http://localhost:9966\n\n");
+        });
+    });
+};
